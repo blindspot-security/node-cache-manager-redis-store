dc026aff1326cdf93175f858d09ccfe66e9387b5
noamilshtein <116799914+noamilshtein@users.noreply.github.com>
2023-12-28 17:31:31 +0200

    fix(all): pretty (#16)
    fix(all): pretty (#16)

--- src/types/RedisStore.interface.ts
+++ src/types/RedisStore.interface.ts
@@ -1,7 +1,7 @@
-import {Store} from "cache-manager";
-import {RedisClientType, RedisDefaultModules, RedisFunctions, RedisModules, RedisScripts} from "redis";
-import {ScanReply} from '@redis/client/dist/lib/commands/SCAN';
-import {RedisCommandRawReply} from "@redis/client/dist/lib/commands";
+import { Store } from "cache-manager";
+import { RedisClientType, RedisDefaultModules, RedisFunctions, RedisModules, RedisScripts } from "redis";
+import { ScanReply } from '@redis/client/dist/lib/commands/SCAN';
+import { RedisCommandRawReply } from "@redis/client/dist/lib/commands";
 
 export interface RedisStore extends Store {
   isCacheableValue: (value: unknown) => boolean;
