6669612dcadd7d73a32d55a82a90ea051251c9ad
noamilshtein <116799914+noamilshtein@users.noreply.github.com>
2023-12-28 17:27:48 +0200

    fix(all) move  get inside atomic func (#15)

--- src/types/RedisStore.interface.ts
+++ src/types/RedisStore.interface.ts
@@ -1,6 +1,7 @@
 import {Store} from "cache-manager";
 import {RedisClientType, RedisDefaultModules, RedisFunctions, RedisModules, RedisScripts} from "redis";
 import {ScanReply} from '@redis/client/dist/lib/commands/SCAN';
+import {RedisCommandRawReply} from "@redis/client/dist/lib/commands";
 
 export interface RedisStore extends Store {
   isCacheableValue: (value: unknown) => boolean;
@@ -9,7 +10,7 @@ export interface RedisStore extends Store {
 
   scan(pattern: string, cursor? :number, count?: number): Promise<ScanReply>;
 
-  atomicGetAndSet(key: string, updateFunction: (val: any) =>  any): Promise<any>;
+  atomicGetAndSet(key: string, updateFunction: (val: any) =>  any): Promise<RedisCommandRawReply>;
 
   flushAll(): Promise<void>
 
